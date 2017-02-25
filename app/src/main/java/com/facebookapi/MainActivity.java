3f800f61f443d16b4d1fe6d9689698062c7c276b
care4u <sarveshchavan777@gmail.com>
2017-02-25 07:31:22 -0800

    modified commit

--- app/src/main/java/com/facebookapi/MainActivity.java
+++ app/src/main/java/com/facebookapi/MainActivity.java
@@ -1,16 +1,15 @@
 package com.facebookapi;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookActivity;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
@@ -18,13 +17,19 @@ import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.facebook.share.model.ShareHashtag;
-import com.facebook.share.model.ShareLinkContent;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     LoginButton lb;
     TextView tv;
     CallbackManager callbackManager;
+    Button post;
+    String x;
+    private static final String PAGE_ID = "288637851555189";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +37,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lb = (LoginButton) findViewById(R.id.button_fb);
         tv = (TextView) findViewById(R.id.text);
+        post = (Button) findViewById(R.id.post);
+
         callbackManager = CallbackManager.Factory.create();
         lb.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                tv.setText("login successful\n" + loginResult.getAccessToken().getApplicationId() + "\n" +
-                        loginResult.getAccessToken().getToken());
+                tv.setText("login successful\n" + loginResult.getAccessToken().getUserId() + "\n" + loginResult.getAccessToken().getToken());
+                x = loginResult.getAccessToken().getToken();
+
             }
 
             @Override
@@ -50,27 +58,64 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String text = "yomen";
+                new GraphRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        "/me/accounts",
+                        null,
+                        HttpMethod.GET,
+                        new GraphRequest.Callback() {
+                            public void onCompleted(GraphResponse response) {
+                                Bundle params = new Bundle();
+                                params.putString("message", text);
+
+                                JSONObject JsonObject = response.getJSONObject();
+                                try {
+                                    JSONArray data = JsonObject.getJSONArray("data");
+                                    JSONObject objectdata = data.getJSONObject(0);
+                                    String accessToken = x;
+                                    Log.d("AccessToken: ", accessToken);
+                                    AccessToken accessToken_obj = new AccessToken(accessToken, AccessToken.getCurrentAccessToken().getApplicationId(),
+                                            AccessToken.getCurrentAccessToken().getUserId(), AccessToken.getCurrentAccessToken().getPermissions(),
+                                            AccessToken.getCurrentAccessToken().getDeclinedPermissions(), AccessToken.getCurrentAccessToken().getSource(),
+                                            AccessToken.getCurrentAccessToken().getExpires(), AccessToken.getCurrentAccessToken().getLastRefresh());
+                                    new GraphRequest(accessToken_obj, "/" + PAGE_ID + "/feed", params, HttpMethod.POST,
+                                            new GraphRequest.Callback() {
+                                                public void onCompleted(GraphResponse response) {
+                                                    Toast.makeText(MainActivity.this, "called", Toast.LENGTH_SHORT).show();
+                                                    if (response.getError() == null)
+
+                                                        Toast.makeText(MainActivity.this, "Shared on facebook page.", Toast.LENGTH_LONG).show();
+                                                    else {
+                                                        Toast.makeText(MainActivity.this, response.getError().getErrorMessage(), Toast.LENGTH_LONG).show();
+                                                        Log.d(response.getError().getErrorMessage(), "Error Message :");
+                                                    }
+                                                }
+                                            }).executeAsync();
+                                } catch (JSONException j) {
+                                    Log.d(j.getMessage(), "Error Message");
+                                   /* Toast.makeText(MainActivity.this, j.getMessage(), Toast.LENGTH_SHORT).show();*/
+                                    Toast.makeText(MainActivity.this, "you should join group before posting", Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }
+                ).executeAsync();
+
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+      /*  Intent intent=new Intent(MainActivity.this,gogo.class);
+        startActivity(intent);*/
     }
 
-    public void gogo(View view) {
-        Bundle params = new Bundle();
-        params.putString("message", "This is a test message");
-/* make the API call */
-        new GraphRequest(
-                AccessToken.getCurrentAccessToken(),
-                "/{1357616140975878}/feed",
-                params,
-                HttpMethod.POST,
-                new GraphRequest.Callback() {
-                    public void onCompleted(GraphResponse response) {
-            /* handle the result */
-                    }
-                }
-        ).executeAsync();
-    }
+
 }
--- app/src/main/java/com/facebookapi/MyAppilcation.java
+++ app/src/main/java/com/facebookapi/MyAppilcation.java
@@ -1,39 +1,38 @@
 package com.facebookapi;
 
+
 import android.app.Application;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
-import android.content.res.Configuration;
 import android.util.Base64;
 import android.util.Log;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-
-
-public class MyAppilcation extends Application {
-    @Override
-    public void onCreate() {
+public class MyAppilcation extends Application{
+@Override
+public void onCreate() {
         super.onCreate();
         printHashKey();
-    }
-    public void printHashKey(){
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.facebookapi",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("sarvesh", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
         }
+public void printHashKey() {
+    // Add code to print out the key hash
+    try {
+        PackageInfo info = getPackageManager().getPackageInfo(
+                "com.facebookapi",
+                PackageManager.GET_SIGNATURES);
+        for (Signature signature : info.signatures) {
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            md.update(signature.toByteArray());
+            Log.d("sarvesh", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+        }
+    } catch (PackageManager.NameNotFoundException e) {
+
+    } catch (NoSuchAlgorithmException e) {
+
     }
+
+}
 }
